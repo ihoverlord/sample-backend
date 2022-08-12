1f1c4bf639d07ce8bb8172eec8b6a3f17f92d416
Himakar Reddy <20149274+ihoverlord@users.noreply.github.com>
2022-08-12 22:04:13 +0530

    Create app.test.js

---
+++ app.test.js
@@ -0,0 +1,9 @@
+const expect = require('chai').expect;
+
+describe('Testing sample Functions', function() {
+  it('1. Should pass the test', function(done) {
+    expect(1+1).to.equal(2);
+    done();
+  });
+
+});
